32764c7555c9c1433a84125ff728cb8fe28f7456
Marco <marco_live@live.com>
2024-02-06 09:44:59 +0000

    PID implemented, write BLE implemented

--- Flight_Controller/Flight_Controller.ino
+++ Flight_Controller/Flight_Controller.ino
@@ -6,12 +6,24 @@
 
 #define BLE_UUID_AHRS_SERVICE                    "F000AB30-0451-4000-B000-000000000000"
 #define BLE_UUID_AHRS_DATA                       "F000AB31-0451-4000-B000-000000000000"
+#define BLE_UUID_AHRS_DATA                       "F000AB32-0451-4000-B000-000000000000"
+#define EULER_ANGLES_SIZE 32
+#define DRONE_DEFAULT_DEVICE_NAME   "DroneTest"
+#define PID_HZ 25
 
+typedef union
+{
+  EulerAngles values;
+  uint8_t bytes[ EULER_ANGLES_SIZE ];
+} EulerAngles_ut;
+
+#pragma region Function prototypes
 //function prototypes
 void InitialiseIMU();
 void InitialiseAHRS();
 bool InitialiseBLE();
-
+#pragma endregion
+#pragma region Obj declaration
 //IMU class obj configuration
 MyBoschSensor myIMU(Wire1);
 //Sensor fusion and Sensor filter used to get attitude and heading 
@@ -19,37 +31,42 @@ ReefwingAHRS ahrs;
 //sensor data obj
 SensorData data;
 //Servo obj -- used to controll ESC
-Servo ESC;
+Servo leftProp;
+Servo rigthProp;
+//PID obj Initialise
+FastPID myPID;
+EulerAngles_ut EulerAnglesData;
+EulerAngles_ut EulerAnglesSetPointData;
+
 
 //BLE variable declaration
-
-#define DRONE_DEFAULT_DEVICE_NAME   "DroneTest"
-
-EulerAngles_ut EulerAnglesData;
-
 BLEService ahrsService( BLE_UUID_AHRS_SERVICE );
 BLECharacteristic ahrsDataCharacteristic( BLE_UUID_AHRS_DATA, BLERead | BLENotify, sizeof EulerAnglesData.bytes );
-
+BLECharacteristic ahrsSetPointDataCharacteristic( BLE_UUID_AHRS_DATA, BLERead | BLEWrite, sizeof EulerAnglesSetPointData.bytes );
+#pragma endregion
+#pragma region variable Declarations
 //PID variable declaration
-float Kp=1, Ki=0.5, Kd=0.25, Hz=50;
-int output_bits = 8;
-bool output_signed = false;
-uint8_t output;
-double setPoint = 25;
+float Kp=1, Ki=0.5, Kd=0.25;
+int output_bits = 16;
+bool output_signed = true;
+int output;
+double setPoint = 1;
 
-//PID obj Initialise
-FastPID myPID(Kp, Ki, Kd, Hz, output_bits, output_signed);
-
-//ESC control variable
-uint8_t outputESC;
-uint8_t outputESCTest;
+int outputLeftProp;
+int outputRightProp;
+int outputleftPropTest;
+double throttle=1300;//init val of prop 
 
 //  Display and Loop Frequency
 int loopFrequency = 0;
 const long displayPeriod = 5000;
 unsigned long previousMillis = 0;
 unsigned long previousMillisPID = 0;
-unsigned long periodOfPID = 0;
+unsigned long loopPeriod = 0;
+
+uint8_t value[32];
+void* ptr;
+#pragma endregion
 
 void setup() {
     //  Start Serial and wait for connection
@@ -58,8 +75,17 @@ void setup() {
 
     InitialiseAHRS();
 
-    //set up the servo/esc
-    ESC.attach(9,1000,2000); // (pin, min pulse width, max pulse width in microseconds)
+    myPID.configure(Kp, Ki, Kd, PID_HZ, output_bits, output_signed);
+    myPID.setOutputRange(-1000,1000);
+    
+
+    //set up the servo/leftProp
+    //send the min signal to Initialise leftProp before connecting battery
+    leftProp.attach(9,1000,2000); // (pin, min pulse width, max pulse width in microseconds)
+    rigthProp.attach(8,1000,2000); // (pin, min pulse width, max pulse width in microseconds)
+    leftProp.writeMicroseconds(1000);
+    rigthProp.writeMicroseconds(1000);
+    //delay(10000);
 
     InitialiseIMU();
 
@@ -68,6 +94,17 @@ void setup() {
         digitalWrite( LED_BUILTIN, HIGH );
         delay(1000);
         digitalWrite( LED_BUILTIN, LOW );
+    }else
+    {
+        digitalWrite( LED_BUILTIN, HIGH );
+        delay(1000);
+        digitalWrite( LED_BUILTIN, LOW );
+        delay(1000);
+        digitalWrite( LED_BUILTIN, HIGH );
+        delay(1000);
+        digitalWrite( LED_BUILTIN, LOW );
+        delay(1000);
+        digitalWrite( LED_BUILTIN, HIGH );
     }
 }
 
@@ -76,60 +113,51 @@ void loop() {
   if (myIMU.accelerationAvailable()) {  myIMU.readAcceleration(data.ax, data.ay, data.az);  }
   if (myIMU.magneticFieldAvailable()) {  myIMU.readMagneticField(data.mx, data.my, data.mz);  }
 
+  //update predictive filter
   ahrs.setData(data);
   ahrs.update();
 
-  periodOfPID = millis() - previousMillisPID;
+  loopPeriod = millis() - previousMillisPID;
 
-  if(periodOfPID >= 20){
+  if(loopPeriod >= (1000/PID_HZ)){
     output = myPID.step(setPoint, ahrs.angles.roll);
+    outputLeftProp = constrain(throttle+output,1000,2000); 
+    outputRightProp = constrain(throttle-output,1000,2000); 
+    
+    // Serial.print("Roll: ");
+    // Serial.print(ahrs.angles.roll);
+    // Serial.print("\tOutput: ");
+    // Serial.print(output);
     // Serial.print("\tLoop Frequency: ");
-    // Serial.print(periodOfPID);
-    // Serial.println(" ms");
-
+    // Serial.print(loopPeriod);
+    // Serial.print(" ms");
+    // Serial.print("\toutputRightProp= ");
+    // Serial.print(outputRightProp);
+    // Serial.print("\toutputleftProp= ");
+    // Serial.print(outputLeftProp);
+    // Serial.print("\tMapped= ");
+    // Serial.println(constrain(output,1000,2000));
     loopFrequency = 0;
     previousMillisPID = millis();
 
   }
 
+  //TODO: decide on a AHRS loop Frequency and also for BLE
   if (millis() - previousMillis >= 1) {
-    //  Display sensor data every displayPeriod, non-blocking.
-
-    outputESC =  map(output, 0, 255, 0, 180); 
-    outputESCTest = map(ahrs.angles.roll,-90,90,0,180);
-    ESC.write(outputESC);
+  
+    BLE.poll();
     EulerAnglesData.values = ahrs.angles;
 
     BLEDevice central = BLE.central();
+  
 
     if (central)
     {
-        Serial.print( "Connected to central: " );
-        Serial.println( central.address() );
+        // Serial.print( "Connected to central: " );
+        // Serial.println( central.address() );
         ahrsDataCharacteristic.writeValue( EulerAnglesData.bytes, sizeof EulerAnglesData.bytes );
     }
-    
-    Serial.print("Roll= ");
-    Serial.println(EulerAnglesData.values.roll,HEX);
-    // Serial.print(" SetPoint= ");
-    // Serial.print(ahrs.angles.pitch);
-    // Serial.print(" outputPID= ");
-    // Serial.print(output);
-    // Serial.print(" outputESCTest= ");
-    // Serial.println(outputESCTest);
 
-    // Serial.print("\tPitch= ");
-    // Serial.print(ahrs.angles.pitch, 2);
-    // Serial.print("\tYaw= ");
-    // Serial.print(ahrs.angles.yaw, 2);
-    // Serial.print("\tHeading= ");
-    // Serial.println(ahrs.angles.heading, 2);
-    //Serial.print("\tLoop Frequency: ");
-    //Serial.print(loopFrequency);
-    //Serial.println(" Hz");
-
-    // loopFrequency = 0;
-    // previousMillis = millis();
   }
 
   loopFrequency++;
@@ -141,19 +169,22 @@ bool InitialiseBLE()
     {
         return false;
     }
+    //BLE.setBinaryConfirmPairing(true);
     // set advertised local name and service UUID:
     BLE.setDeviceName( DRONE_DEFAULT_DEVICE_NAME );
     BLE.setLocalName( DRONE_DEFAULT_DEVICE_NAME );
     BLE.setAdvertisedService( ahrsService );
+    BLE.setPairable(true);
 
+    ahrsSetPointDataCharacteristic.setEventHandler(BLEWritten,CallBackSetPointWritten);
     // BLE add characteristics
-    ahrsService.addCharacteristic( ahrsDataCharacteristic );
-    BLE.addService( ahrsService );
+    ahrsService.addCharacteristic(ahrsDataCharacteristic);
+    ahrsService.addCharacteristic(ahrsSetPointDataCharacteristic);
+    BLE.addService(ahrsService);
     ahrsDataCharacteristic.writeValue( EulerAnglesData.bytes, sizeof EulerAnglesData.bytes );
 
-      // start advertising
+    // start advertising
     BLE.advertise();
-
     return true;
 }
 
@@ -196,4 +227,16 @@ void InitialiseIMU(){
     while(1);
   }
 
+}
+
+void CallBackSetPointWritten(BLEDevice central, BLECharacteristic characteristic){
+  int i = 0;
+  Serial.print("Characteristic event, written: ");
+  if (characteristic.value())
+  {
+    
+    characteristic.readValue(EulerAnglesSetPointData.bytes,sizeof(EulerAnglesSetPointData.bytes));
+    Serial.print(EulerAnglesSetPointData.values.roll);
+  }
+  Serial.println("\tEnd of Function");
 }
